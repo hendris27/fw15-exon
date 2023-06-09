223714a66f9709044c51ddaf8e8d404b4e1bae0a
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-09 16:18:36 +0700

    fix aside

--- src/pages/search-receiver.jsx
+++ src/pages/search-receiver.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { RxDashboard } from 'react-icons/rx';
 import { BsSearch } from 'react-icons/bs';
-import { AiOutlineArrowUp, AiOutlineUser, AiOutlineLogout, AiOutlinePlus } from 'react-icons/ai';
 import Headers from '@/components/header';
 import Footers from '@/components/footers';
+import Aside from '@/components/aside';
+
 import Image from 'next/image';
 import default_picture from '../assets/img/default.jpg';
 
@@ -13,40 +13,7 @@ export default function Home() {
     <div className="h-screen">
       <Headers />
       <div className="flex bg-[#E8F6EF] px-[100px] py-[50px] gap-4 h-full ">
-        <div className="bg-white shadow-2xl w-[30%] py-8 px-6 rounded-3xl flex flex-col  justify-between ">
-          <div className="flex flex-col gap-8">
-            <div className="flex gap-8 ">
-              <div>
-                <RxDashboard size={25} color="#69BEB9" />
-              </div>
-              <div className="font-bold text-[#69BEB9]">Dashboard</div>
-            </div>
-            <div className="flex gap-8 ">
-              <div>
-                <AiOutlineArrowUp size={25} color="#69BEB9" />
-              </div>
-              <div className="font-bold text-[#69BEB9]">Transfer</div>
-            </div>
-            <div className="flex gap-8 ">
-              <div>
-                <AiOutlinePlus size={25} color="#69BEB9" />
-              </div>
-              <div className="font-bold text-[#69BEB9]">Top Up</div>
-            </div>
-            <div className="flex gap-8 ">
-              <div>
-                <AiOutlineUser size={25} color="#69BEB9" />
-              </div>
-              <div className="font-bold text-[#69BEB9]">Profile</div>
-            </div>
-          </div>
-          <div className="flex gap-8 ">
-            <div>
-              <AiOutlineLogout size={25} color="red" />
-            </div>
-            <div className="font-bold text-[#FF0000]">Logout</div>
-          </div>
-        </div>
+        <Aside />
         <div className="flex-1 bg-white rounded-3xl flex flex-col gap-4 p-8">
           <div className="flex justify-between">
             <div className="font-bold">Search Receiver</div>
@@ -55,10 +22,10 @@ export default function Home() {
             <div className="flex w-[5%]">
               <BsSearch size={25} />
             </div>
-            <div className="flex-1 w-full">
+            <div className=" flex-1 w-full">
               <input
                 type="text"
-                className="input bg-gray-100 border-0 outline-none hover:border-0 hover:outline-none"
+                className="w-full input bg-gray-100 border-0 outline-none hover:border-0 hover:outline-none"
                 placeholder="Search receiver here"
               />
             </div>
