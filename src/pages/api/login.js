bfc8cb753fe8669c4801a7884675ceb5be755aec
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-12 00:14:39 +0700

    update file api login

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,35 +1,19 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
+import cookieConfig from '@/assets/helpers/cookieConfig';
 
-export default withIronSessionApiRoute(
-  async function loginRoute(req, res) {
-    // // get user from database then:
-    // req.session.user = {
-    //   id: 230,
-    //   admin: true,
-    // };
-    // await req.session.save();
-    // res.send({ ok: true });
-    const request = await fetch('https://cute-lime-goldfish-toga.cyclic.app', {
-      method: 'POST',
-      body: new URLSearchParams(req.body).toString(),
-      headers: {
-        'Content-Type': 'application/x-www-form-url-encoded',
-      },
-    });
-    const response = await request.json();
-    const token = response?.results?.token;
-    if (token) {
-      req.session.token = token;
-      await req.session.save();
-    }
-    return res.json(response);
-  },
-  {
-    cookieName: 'exonpay',
-    password: 'cmX56pNL%2W&HB8cUzVux9lR&23*a2K4r',
-    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-    cookieOptions: {
-      secure: process.env.NODE_ENV === 'production',
+export default withIronSessionApiRoute(async function loginRoute(req, res) {
+  const request = await fetch('https://cute-lime-goldfish-toga.cyclic.app/auth/login', {
+    method: 'POST',
+    body: new URLSearchParams(req.body).toString(),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  },
-);
+  });
+  const response = await request.json();
+  const token = response?.results?.token;
+  if (token) {
+    req.session.token = token;
+    await req.session.save();
+  }
+  return res.json(response);
+}, cookieConfig);
