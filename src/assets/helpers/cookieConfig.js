1fbbcdabac5b63ecfafd600d8066915b2682a0f4
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-12 17:12:34 +0700

    deleted

--- src/assets/helpers/cookieConfig.js
+++
@@ -1,8 +0,0 @@
-const cookieConfig = {
-  cookieName: 'exonpay',
-  password: 'cmX56pNL%2W&HB8cUzVux9lR&23*a2K4r',
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-  },
-};
-export default cookieConfig;
