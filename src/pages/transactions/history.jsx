7e77eafb110b4e727526861cd5f75ce42581b925
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-08-08 16:55:38 +0700

    feat(get data history)

--- src/pages/transactions/history.jsx
+++ src/pages/transactions/history.jsx
@@ -24,11 +24,12 @@ export const getServerSideProps = withIronSessionSsr(async function getServerSid
 function SearchRecipient({ userToken }) {
   const [search, setSearch] = React.useState('');
   const [recipient, setRecepient] = React.useState([]);
+  const [history, setHistory] = React.useState([]);
   const user = useSelector((state) => state.profile.data);
 
   const getTransaction = React.useCallback(async () => {
     const { data } = await http(userToken).get('/transactions', { params: { limit: 5 } });
-    setRecepient(data.results);
+    setHistory(data.results);
     console.log(data);
   }, [userToken]);
 
@@ -93,7 +94,7 @@ function SearchRecipient({ userToken }) {
           </div>
           {recipient.results && (
             <div className="flex flex-col gap-4">
-              {recipient.results.length === 0 ? (
+              {/* {recipient.results.length === 0 ? (
                 <p className="text-[#ff0000] ">No data found in list.</p>
               ) : (
                 recipient.results.map((item) => (
@@ -195,10 +196,110 @@ function SearchRecipient({ userToken }) {
                     </div>
                   </div>
                 ))
-              )}
+              )} */}
+              {history.map((item) => (
+                <div key={`trx-list-${item.id}`} className="flex justify-between px-7 py-2">
+                  <div className="flex gap-2">
+                    {item.type === 'TRANSFER' && (
+                      <>
+                        {item.recipient.id !== user.id && (
+                          <>
+                            <div>
+                              {!item.recipient.picture && (
+                                <div className="w-12 h-12 border rounded-lg flex justify-center items-center">
+                                  <FiUser size={30} />
+                                </div>
+                              )}
+                              {item.recipient.picture && (
+                                <div className="w-12 h-12 border rounded-lg overflow-hidden object-cover ">
+                                  <Image
+                                    className="rounded object-cover"
+                                    src={item.recipient.picture}
+                                    alt={item.recipient.fullName || item.recipient.email}
+                                    width={100}
+                                    height={100}
+                                  />
+                                </div>
+                              )}
+                            </div>
+                            <div className="flex flex-col gap-1">
+                              <div className="font-semibold">{item.recipient.fullName || item.recipient.email}</div>
+                              <div className="text-sm opacity-70 text-red-500 font-bold ">Outcome</div>
+                            </div>
+                          </>
+                        )}
+                        {item.recipient.id === user.id && (
+                          <>
+                            <div>
+                              {!item.sender.picture && (
+                                <div className="w-12 h-12 border rounded-lg flex justify-center items-center">
+                                  <FiUser size={30} />
+                                </div>
+                              )}
+                              {item.sender.picture && (
+                                <div className="w-12 h-12 border rounded-lg overflow-hidden">
+                                  <Image
+                                    className="rounded object-fit "
+                                    src={item.sender.picture}
+                                    alt={item.sender.fullName || item.sender.email}
+                                    width={100}
+                                    height={100}
+                                  />
+                                </div>
+                              )}
+                            </div>
+                            <div className="flex flex-col gap-1">
+                              <div className="font-semibold">{item.sender.fullName || item.sender.email}</div>
+                              <div className="text-sm opacity-70 text-red-500 font-bold">Outcome</div>
+                            </div>
+                          </>
+                        )}
+                      </>
+                    )}
+                    {item.type === 'TOP-UP' && (
+                      <>
+                        <div>
+                          {!item.picture && (
+                            <div className="w-12 h-12 border rounded-lg flex justify-center items-center">
+                              <FiUser size={30} />
+                            </div>
+                          )}
+                          {item.picture && (
+                            <div className="w-12 h-12 border rounded-lg overflow-hidden">
+                              <Image
+                                className="rounded object-fit "
+                                src={item.picture}
+                                alt={item.fullName || item.email}
+                                width={100}
+                                height={100}
+                              />
+                            </div>
+                          )}
+                        </div>
+                        <div className="flex flex-col gap-1">
+                          <div className="font-semibold">{item.recipient.fullName || item.recipient.email}</div>
+                          <div className="text-sm text-green-700 font-bold opacity-70">Income</div>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                  <div>
+                    {item.type === 'TOP-UP' && (
+                      <div className="font-semibold text-green-500">Rp{Number(item.amount).toLocaleString('id')}</div>
+                    )}
+                    {item.type === 'TRANSFER' &&
+                      (item.recipient.id === user.id ? (
+                        <div className="text-green-500">Rp{Number(item.amount).toLocaleString('id')}</div>
+                      ) : (
+                        <div className="text-red-500">Rp{Number(item.amount).toLocaleString('id')}</div>
+                      ))}
+                  </div>
+                </div>
+              ))}
+
               <div className="flex gap-2  justify-center items-center">
                 <button
-                  onClick={() => getUsers(recipient.pageInfo.page - 1, search)}
+                  onClick={() => getTransaction(recipient.pageInfo.page - 1, search)}
                   disabled={recipient.pageInfo.page <= 1}
                   className="btn btn-primary"
                 >
@@ -207,7 +308,7 @@ function SearchRecipient({ userToken }) {
                 <div className="font-bold">
                   page {recipient.pageInfo.page} of {recipient.pageInfo.totalPage}
                 </div>
-                <button onClick={() => getUsers(recipient.pageInfo.page + 1, search)} className="btn btn-primary">
+                <button onClick={() => getTransaction(recipient.pageInfo.page + 1, search)} className="btn btn-primary">
                   <AiOutlineArrowRight />
                 </button>
               </div>
